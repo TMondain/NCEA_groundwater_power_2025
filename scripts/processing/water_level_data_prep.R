ab91acb01493d3d06766c5865f9afa0b7bd006aa
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2025-03-04 14:45:04 +0000

    Adding percentage change

--- scripts/processing/water_level_data_prep.R
+++ scripts/processing/water_level_data_prep.R
@@ -1,4 +1,4 @@
-
+rm(list = ls())
 
 ## water level data prep 
 library(tidyverse)
@@ -73,77 +73,8 @@ top5 <- topn_rows(dataset = dip,
 # get median of each station
 median_5 <- tapply(top5$water_level, top5$station_number, median)
 
-
-median_5df <- as.data.frame(median_5)
-med$site_num <- rownames(med)
-head(med)
-
 dip$baseline5yr <- median_5[match(dip$station_number, names(median_5))]
 
-
-
-
-dip %>%
-  arrange(station_number, year) %>% 
-  group_by(station_number) %>%
-  top_n(5) %>% 
-  arrange(station_number, year)
-
-dip %>% 
-  arrange(station_number, year) %>% 
-  group_by(station_number) %>% 
-  slice(1:5)
-
-dataset = dip 
-group_col = "station_number" 
-sort_col = "year"
-nrows_to_keep = 5
-
-
-
-tst <- topn_rows(dataset = dip,
-                 group_col = "station_number",
-                 sort_col = "year",
-                 nrows_to_keep = 5)
-
-
-
-
-# get top 5 years of data for each site
-top_5yrs <- do.call(rbind, lapply(sites, function(x) {
-  
-  # select the station of interest
-  sitesub <- dip[dip$station_number == x,]
-  
-  # sort by year, select top 5  
-  first_yrs <- sort(unique(sitesub$year))[1:5]
-  
-  # get only first 5yrs' worth data
-  sitesub[sitesub$year %in% first_yrs,]
-  
-}))
-
-tst <- arrange(d2, station_number, year)
-
-head(tst)
-
-head(top_5yrs, 100)
-
-hist(top_5yrs$water_level)
-
-# get 5yr median per group
-median_h2o5 <- tapply(top_5yrs$water_level, top_5yrs$station_number, median)
-head(median_h2o5)
-names(median_h2o5)
-
-hist(median_h2o5)
-
-med <- as.data.frame(median_h2o5)
-med$site_num <- rownames(med)
-head(med)
-
-dip$yr_base5 <- med$median_h2o5[match(dip$station_number, med$site_num)]
-head(dip)
-
-identical(dip$yr_base5, dip$baseline5yr)
-dip[dip$yr_base5 != dip$baseline5yr,]
+# calculate % of 5yr base
+dip <- dip %>% 
+  mutate(perc_chg = water_level/baseline5yr*100)
