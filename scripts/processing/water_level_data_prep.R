dc860014487633e9107798399d72727e883f0001
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2025-03-05 07:49:58 +0000

    gettin 5yr median for site data too

--- scripts/processing/water_level_data_prep.R
+++ scripts/processing/water_level_data_prep.R
@@ -16,65 +16,135 @@ mn_dip <- mean(dip$water_level)
 mn_site <- mean(site$water_level)
 
 
-#### Step 2: calculate 5th centile
+#### Step 2: calculate 5th centile ---------------------------------------------
 
-# remove negative values
-dip <- dip[dip$water_level >= 0,]
+## dip data
+hist(dip$water_level)
+
+# remove negative values?
+# dip <- dip[dip$water_level >= 0,]
 head(dip)
 
-head(dip, 50) %>% 
-  dplyr::group_by(station_number, station_name, year) %>% 
-  dplyr::summarise(cent_5 = quantile(water_level, probs = 0.05, na.rm = TRUE),
+dip_quantsum <- dip %>% 
+  dplyr::group_by(station_number, station_name, easting, northing, year) %>% 
+  dplyr::summarise(median_val = median(water_level, na.rm = TRUE),
+                   cent_5 = quantile(water_level, probs = 0.05, na.rm = TRUE),
                    cent_95 = quantile(water_level, probs = 0.95, na.rm = TRUE))
 
 
+## site data
+hist(site$water_level)
+
+# remove negative values?
+# site <- site[site$water_level >= 0,]
+head(site)
+
+site_quantsum <- site %>% 
+  dplyr::group_by(station_number, station_name, easting, northing, year) %>% 
+  dplyr::summarise(median_val = median(water_level, na.rm = TRUE),
+                   cent_5 = quantile(water_level, probs = 0.05, na.rm = TRUE),
+                   cent_95 = quantile(water_level, probs = 0.95, na.rm = TRUE))
 
 
+## save
+dir.create("data/processed")
 
-# 5yr baseline by site
+write.csv(dip_quantsum, 
+          "data/processed/dip_data_quantile_summary.csv")
 
+write.csv(site_quantsum, 
+          "data/processed/site_data_quantile_summary.csv")
 
-# how many years per site
+#### Step 3: %chg relative to 5yr baseline by site -----------------------------
+
+## dip
+# check how many years per site?
 dip %>% 
   group_by(station_number) %>% 
   summarise(length(unique(year)))
 ## not the same number of years at each site
+## decision: make the baseline the first 5 yrs worth of data at that site?
 
-# function to keep the first n rows of a group
-topn_rows <- function(dataset, group_col, sort_col, nrows_to_keep) {
+# function to keep the data associated with the first n groups
+firstn_groups <- function(dataset, group_col, sort_col, 
+                          nrows_to_keep, sort_decreasing = FALSE) {
   
   # get unique sites
   uniq_group <- unique(dataset[,group_col])
   
-  # get top n rows of data for each group
-  top_nyrs <- do.call(rbind, lapply(uniq_group, function(x) {
+  # get data associated with first n groups
+  first_groupdat <- do.call(rbind, lapply(uniq_group, function(x) {
     
     # select the station of interest
     sitesub <- dataset[dataset[,group_col] == x,]
     
-    # sort by year, select top 5  
-    topn_rows <- sort(unique(sitesub[,sort_col]))[1:nrows_to_keep]
+    # sort by group, select first 5 groups
+    groups_to_keep <- sort(unique(sitesub[,sort_col]), 
+                           decreasing = sort_decreasing)[1:nrows_to_keep]
     
     # get only first 5yrs' worth data
-    sitesub[sitesub[,sort_col] %in% topn_rows,]
+    sitesub[sitesub[,sort_col] %in% groups_to_keep,]
     
   }))
   
-  top_nyrs
+  first_groupdat
   
 }
 
 ## use this to extract the data for the first 5 years for each station
-top5 <- topn_rows(dataset = dip,
-                  group_col = "station_number",
-                  sort_col = "year",
-                  nrows_to_keep = 5)
+first5 <- firstn_groups(dataset = dip,
+                        group_col = "station_number",
+                        sort_col = "year",
+                        nrows_to_keep = 5, 
+                        sort_decreasing = FALSE)
 
-# get median of each station
-median_5 <- tapply(top5$water_level, top5$station_number, median)
+# get median of the first 5 years for each station
+median_5 <- tapply(first5$water_level, first5$station_number, median)
 
 dip$baseline5yr <- median_5[match(dip$station_number, names(median_5))]
 
 # calculate % of 5yr base
 dip <- dip %>% 
-  mutate(perc_chg = water_level/baseline5yr*100)
+  mutate(perc_chg_ind = water_level/baseline5yr*100)
+
+head(dip)
+hist(dip$perc_chg_ind)
+hist(dip$perc_chg_ind[dip$perc_chg_ind<500])
+
+
+## site
+# check how many years per site?
+site %>% 
+  group_by(station_number) %>% 
+  summarise(length(unique(year)))
+## not the same number of years at each site
+## decision: make the baseline the first 5 yrs worth of data at that site?
+
+## use this to extract the data for the first 5 years for each station
+first5_site <- firstn_groups(dataset = site,
+                             group_col = "station_number",
+                             sort_col = "year",
+                             nrows_to_keep = 5, 
+                             sort_decreasing = FALSE)
+
+# get median of the first 5 years for each station
+median_5site <- tapply(first5_site$water_level, first5_site$station_number, median)
+
+site$baseline5yr <- median_5site[match(site$station_number, names(median_5site))]
+
+# calculate % of 5yr base
+site <- site %>% 
+  mutate(perc_chg_ind = water_level/baseline5yr*100)
+
+head(site)
+hist(site$perc_chg)
+hist(site$perc_chg[site$perc_chg<500])
+
+
+## save
+write.csv(dip,
+          "data/processed/dip_data_5yr_perc_change.csv")
+
+write.csv(site,
+          "data/processed/site_data_5yr_perc_change.csv")
+
