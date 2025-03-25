ebd08eaab9bddc3b5c9f180b5c9e5b05f2ad1906
Thomas Mondain-Monval <thoval@ceh.ac.uk>
2025-03-25 11:38:19 +0000

    changing name of function

--- scripts/analyses/testing_new_pwr_function.R
+++ scripts/analyses/testing_new_pwr_function.R
@@ -1,5 +1,8 @@
 # testing new function
-source("scripts/functions/water_level_power_analysis.R")
+source("scripts/functions/run_power_analysis.R")
+source("scripts/functions/power_subfunctions.R")
+
+
 
 # submitting >1 dataset
 datasets = paste("data/processed/dip_data_mn_5yrperc_change.csv", 
